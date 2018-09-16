b6a5509618ebc6ae3edd4379a92fda0b0af1bc6b
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-09-16 10:27:10 +0300

    fix url to api

--- src/Interface/User/user.service.ts
+++ src/Interface/User/user.service.ts
@@ -6,13 +6,13 @@ import {Observable} from "rxjs";
 @Injectable()
 export class UserService {
 
-  private apiHost: string = 'http://127.0.0.1/:8080';
+  private apiHost: string = 'http://127.0.0.1:8080';
 
   constructor(private http: Http) {
   }
 
   public login(email:string, password:string):Observable<any> {
-    return this.http.get(this.apiHost+"/api/authenticate").map((response) => {
+    return this.http.get(this.apiHost+"/authenticate").map((response) => {
       return response.json();
     })
       .catch((err) => {
