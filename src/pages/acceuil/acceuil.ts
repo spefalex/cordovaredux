c64afd6b6b52e2e77e3787c85a7d12f75ecd7495
Rakotondratsirahonina <rakoto.tsira@gmail.com>
2018-09-21 14:00:32 +0300

    add function getphoto

--- src/pages/acceuil/acceuil.ts
+++ src/pages/acceuil/acceuil.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController,
+  LoadingController
+} from "ionic-angular";
 import { StorageService } from "../../Interface/Services/storage-service";
+import { ImagePicker } from "@ionic-native/image-picker";
+import { Base64 } from "@ionic-native/base64";
 /**
  * Generated class for the AcceuilPage page.
  *
@@ -10,19 +18,43 @@ import { StorageService } from "../../Interface/Services/storage-service";
 
 @IonicPage()
 @Component({
-  selector: 'page-acceuil',
-  templateUrl: 'acceuil.html',
+  selector: "page-acceuil",
+  templateUrl: "acceuil.html"
 })
 export class AcceuilPage {
-  imgPreview = 'assets/imgs/blank-avatar.jpg';
-  constructor(public navCtrl: NavController, public navParams: NavParams, private storageService: StorageService) {
-  }
+  imgPreview = "assets/imgs/blank-avatar.jpg";
+  loading: any;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private storageService: StorageService,
+    private imagePicker: ImagePicker,
+    private base64: Base64,
+    public loadingCtrl: LoadingController
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AcceuilPage');
-    this.storageService.get('idUser').then(data => {
-     console.log(data)
+    console.log("ionViewDidLoad AcceuilPage");
+    this.storageService.get("idUser").then(data => {
+      console.log(data);
     });
   }
 
+  getPhoto() {
+    let options = {
+      maximumImagesCount: 1
+    };
+    this.imagePicker.getPictures(options).then((results) => {
+      for (var i = 0; i < results.length; i++) {
+          this.imgPreview = results[i];
+          this.base64.encodeFile(results[i]).then((base64File: string) => {
+          //  this.regData.avatar = base64File;
+          console.log('avatar',base64File);
+         
+        }, (err) => {
+            console.log(err);
+          });
+      }
+    }, (err) => { });
+  }
 }
